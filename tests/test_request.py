1b79877aa2f952a760498a53268a403968139cac
Miguel Figueira Ferraz <miguelfigueiraferraz@gmail.com>
2023-11-06 14:54:10 -0300

    test: Leverages `@pytest.fixture`

--- tests/test_request.py
+++ tests/test_request.py
@@ -1,73 +1,38 @@
+import pytest
 from unittest.mock import MagicMock
-from wc_client.request import WCRequest  # Import your WCRequest class
+from wc_client.request import WCRequest
 
 
-# Mocked response for HTTP request
 class MockResponse:
     def __init__(self, content, status_code=200):
         self.content = content
         self.status_code = status_code
 
 
-def test_build_url():
-    request = WCRequest(
+@pytest.fixture
+def wc_request():
+    return WCRequest(
         "https://example.com",
         {"Accept": "application/json"},
         "consumer",
         "orders",
         "1",
     )
+
+
+# Test cases
+def test_build_url(wc_request):
     expected_url = "https://example.com/consumer/orders/1"
-    assert request._build_url() == expected_url
+    assert wc_request._build_url() == expected_url
 
 
-def test_update_headers():
-    request = WCRequest(
-        "https://example.com",
-        {"Accept": "application/json"},
-        "consumer",
-        "orders",
-        "1",
-    )
-    request._update_headers({"Authorization": "Bearer token"})
+def test_update_headers(wc_request):
+    wc_request._update_headers({"Authorization": "Bearer token"})
     expected_headers = {
         "Accept": "application/json",
         "Authorization": "Bearer token",
     }
-    assert request.headers == expected_headers
-
-
-def test_make_request():
-    # Mocking httpx.Client.request
-    client = MagicMock()
-    client.request.return_value = MockResponse(b'{"key": "value"}', 200)
-    request = WCRequest(
-        "https://example.com",
-        {"Accept": "application/json"},
-        "consumer",
-        "orders",
-        "1",
-    )
-    request.client = client
-
-    response = request.get(
-        body=None,
-        query_params=None,
-        headers={"Authorization": "foo-bar"},  # update headers
-    )
-
-    client.request.assert_called_once_with(
-        method="get",
-        url="https://example.com/consumer/orders/1",
-        data=None,
-        params=None,
-        headers={
-            "Accept": "application/json",
-            "Authorization": "foo-bar",
-        },  # assert updated headers
-    )
-    assert response.status_code == 200
-    assert response.content == b'{"key": "value"}'
+    assert wc_request.headers == expected_headers
 
 
 def test_chained_calls():
@@ -83,3 +48,29 @@ def test_chained_calls():
     new_request = new_request.details
     expected_url = "https://example.com/consumer/orders/1/details"
     assert new_request._build_url() == expected_url
+
+
+def test_make_request(wc_request):
+    # Mocking httpx.Client.request
+    client = MagicMock()
+    client.request.return_value = MockResponse(b'{"key": "value"}', 200)
+    wc_request.client = client
+
+    response = wc_request.get(
+        body=None,
+        query_params=None,
+        headers={"Authorization": "foo-bar"},
+    )
+
+    client.request.assert_called_once_with(
+        method="get",
+        url="https://example.com/consumer/orders/1",
+        data=None,
+        params=None,
+        headers={
+            "Accept": "application/json",
+            "Authorization": "foo-bar",
+        },  # Assert updated headers
+    )
+    assert response.status_code == 200
+    assert response.content == b'{"key": "value"}'
